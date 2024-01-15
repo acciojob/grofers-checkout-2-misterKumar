c8648efab92c5b3d6a9eeda7ac4712983a63035b
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-15 14:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+const priceArray=document.querySelectorAll('[data-ns-test="prices"]');
+let table=document.getElementById("table1");
+let ans=0;
+for (let i = 0; i < priceArray.length; i++) {
+	ans+=parseInt(priceArray[i].innerText);
+}
+let total=document.createElement("tr");
+
+total.setAttribute("data-ns-test", "grandTotal");
+
+total.innerText=ans;
+
+table.appendChild(total);
